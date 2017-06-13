9dcf2b9374b44681f5ae7fb70fa921a2985b9946
Dmitry Ryumkin <dima@Dmitrys-MacBook-Pro.local>
2017-06-13 15:58:16 +0300

    Change tests

---
+++ test/association_test.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+describe('Associations', () => {
+  let joe, blogPost, comment;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep' });
+    comment = new Comment({ content: 'Congrats on new post'});
+  });
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -13,8 +13,13 @@ before((done) => {
 });
 
 beforeEach((done) => {
-    mongoose.connection.collections.users.drop(() => {
-        done();
+    const { users, comments, blogPosts } = mongoose.connection.collections;
+    users.drop(() => {
+       comments.drop(() => {
+           blogposts.drop(() => {
+               done();
+           })
+       }) 
     });
 });
 
